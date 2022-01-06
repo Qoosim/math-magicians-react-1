87f05b103878926482effa61027bb57bcb89c3a1
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-01-06 18:00:54 +0100

    Add state to the App.js, add handleClick function to set the setState and add event listener to all the button elements

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,56 @@
 import React from 'react';
+import calculate from '../logic/calculate';
+import operate from '../logic/operate'; // eslint-disable-line
 
 class Calculator extends React.Component { // eslint-disable-line
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (event) => {
+    this.setState((prevState) => ({
+      data: calculate(prevState.data, event.target.innerText),
+    }));
+  }
+
   render() {
+    const { data } = this.state;
     return (
       <div className="calc-grid">
         <div className="output-div">
-          <div className="output">0</div>
+          <div className="output">
+            {(data.total ? data.total : '') + (data.operation ? data.operation : '') + (data.next ? data.next : '') || 0}
+          </div>
         </div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operation">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="operation">×</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="operation">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="operation">+</button>
-        <button type="button" className="span-two">0</button>
-        <button type="button">.</button>
-        <button type="button" className="operation">=</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>AC</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>+/-</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>%</button>
+        <button type="button" onClick={(event) => this.handleClick(event)} className="operation">÷</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>7</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>8</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>9</button>
+        <button type="button" onClick={(event) => this.handleClick(event)} className="operation">x</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>4</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>5</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>6</button>
+        <button type="button" onClick={(event) => this.handleClick(event)} className="operation">-</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>1</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>2</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>3</button>
+        <button type="button" onClick={(event) => this.handleClick(event)} className="operation">+</button>
+        <button type="button" onClick={(event) => this.handleClick(event)} className="span-two">0</button>
+        <button type="button" onClick={(event) => this.handleClick(event)}>.</button>
+        <button type="button" onClick={(event) => this.handleClick(event)} className="operation">=</button>
       </div>
     );
   }
