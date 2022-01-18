47ea1a58b8b696fc2e2a60ae98001e26094e14b4
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-01-18 13:15:40 +0100

    Add Quotes component and its content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 import Home from './components/Home';
+import Quotes from './components/Quotes';
 
 class App extends React.Component { // eslint-disable-line
   render() {
     return (
       <>
+        <Quotes />
         <Home />
         <Calculator />
       </>
---
+++ src/components/Quotes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Quotes = () => (
+  <main>
+    <section>
+      <h1 className="quote">Qoute of the day</h1>
+      <p className="quote-msg">
+        Mathematics is not about numbers, equations, computations or algorithm:
+        It is about understanding.
+        <br />
+        **-William Paul Thurston-**
+      </p>
+    </section>
+  </main>
+);
+
+export default Quotes;
