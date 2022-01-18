36d203bca61d0b8eb61625a2f85bb0c08ab29abe
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-01-18 13:11:11 +0100

    Add Home component with content

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Home = () => (
+  <main className="home-container">
+    <section>
+      <h3>Welcome to the home of mathematics!</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Asperiores quaerat cumque magni perspiciatis quam distinctio,
+        natus eaque saepe iure! Molestias facere, libero recusandae
+        sapiente rerum consectetur officiis at hic saepe ipsa voluptate
+        ipsum tenetur voluptatum quia eaque quaerat ullam laborum nisi consequatur,
+        aut labore, quod neque. Illo dolorum sapiente vero reprehenderit inventore,
+        iste in culpa asperiores ad tenetur molestias illum velit maxime voluptatem
+        eum repudiandae eligendi, nisi eaque explicabo, suscipit pariatur.
+        Sed hic pariatur explicabo inventore? Rerum tenetur sapiente, dolorum sequi
+        harum dolores officia! Aliquid, magni in a consequuntur, atque animi provident,
+        labore est placeat velit nemo illo asperiores expedita!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+        Asperiores quaerat cumque magni perspiciatis quam distinctio,
+        natus eaque saepe iure! Molestias facere, libero recusandae
+        sapiente rerum consectetur officiis at hic saepe ipsa voluptate
+        ipsum tenetur voluptatum quia eaque quaerat ullam laborum nisi consequatur,
+        aut labore, quod neque. Illo dolorum sapiente vero reprehenderit inventore,
+        iste in culpa asperiores ad tenetur molestias illum velit maxime voluptatem
+        eum repudiandae eligendi, nisi eaque explicabo, suscipit pariatur.
+        Sed hic pariatur explicabo inventore? Rerum tenetur sapiente, dolorum sequi
+        harum dolores officia! Aliquid, magni in a consequuntur, atque animi provident,
+        labore est placeat velit nemo illo asperiores expedita!
+      </p>
+    </section>
+  </main>
+);
+
+export default Home;
